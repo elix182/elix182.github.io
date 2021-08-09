128aea00fe506f0853338a9352fa787bcd24ba32
Eliezer Pacheco Jiménez <elix182@users.noreply.github.com>
2021-08-08 18:58:26 -0700

    Changes in layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,46 @@
-function hello(){
-    
+window.onload = () => {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+       setTimeout(init, 10);
+   }
+}
+
+const init = () => {
+    const today = new Date();
+    const dateDOM = $('#date');
+    dateDOM.html(today.getFullYear());
+};
+
+const openUrl = (url, target) => {
+    if(!target) {
+        target = '_blank';
+    }
+    window.open(url, target);
+}
+
+const showSection = elementId => {
+    const elementDOM = $(elementId);
+    const sectionsDOM = $('.section.show');
+    sectionsDOM.each(function() {
+        const uiDOM = $(this);
+        uiDOM.removeClass('show');
+        uiDOM.addClass('hide');
+    });
+    elementDOM.removeClass('hide');
+    elementDOM.addClass('show');
+}
+
+const hideSection = elementId => {
+    const elementDOM = $(elementId);
+    elementDOM.removeClass('show');
+    elementDOM.addClass('hide');
+}
+
+const hideSections = () => {
+    const sectionsDOM = $('.section');
+    sectionsDOM.each(function() {
+        const uiDOM = $(this);
+        uiDOM.removeClass('show');
+        uiDOM.addClass('hide');
+    });
 }
